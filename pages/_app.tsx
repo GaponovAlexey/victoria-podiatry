1ba0cf77dd6ca0b4ac9fac3100e8ce5d67694415
Kekelberi <alexei.gaponov@gmail.com>
2022-04-02 15:49:24 +0300

    save

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/reset.css'
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from '../components/Redux/store'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
